914d1ecdfdeca78b1097d05c0c8c203be46f7373
bienculo8282 <139576246+bienculo8282@users.noreply.github.com>
2023-09-24 11:01:58 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -21,7 +21,7 @@ module.exports = {
     return [
       {
         source: '/posts/:slug',
-        destination: 'https://thesenholding.com/:slug',
+        destination: 'https://trochoitapthe.com/:slug',
         has: [
           {
             type: 'header',
